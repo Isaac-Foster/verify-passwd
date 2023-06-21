4888a5099bca1b370f69ff3580c641a91be47e1f
isaac <110870485+Isaac-Foster@users.noreply.github.com>
2023-06-21 12:08:53 -0300

    Update verify.py
    Is new format funcion

--- verify.py
+++ verify.py
@@ -1,24 +1,31 @@
-def verify(passwd) -> tuple:
-    import re
+import re, string
 
-    dg = True if re.search(r"\d+", passwd) else False
-    up = True if re.search(r"[A-Z]+", passwd) else False
-    lc = True if re.search(r"[a-z]+", passwd) else False
-    pt = True if re.search(r"[@#$&*!;]+", passwd) else False
-    d = {"digits": dg, "uppercase": up, "lowercase": lc, "symbol": pt}
-    error = ", ".join([k for k, v in d.items() if v == False]).split(",")
 
-    match len(error):
-        case 1:
-            msg = "".join(error)
-        case 2:
-            msg = " and ".join(error)
-        case 3:
-            msg = ", ".join(error[:2]) + f" and {error[-1]}"
+def is_strong_pass(
+    passwd: str, 
+    chars: int = 10, 
+    lowers: int = 3, 
+    uppers: int = 1, 
+    digits: int = 1
+    ):
 
-    if all([dg, up, lc, pt]) and len([dg, up, lc, pt]) == 4 and len(passwd) > 8:
-        return True, {"message": "your password is strong."}
-    elif len(passwd) < 8:
-        return False, {"message": "your password need min 8 char"}
-    else:
-        return False, {"message": f"your password need {msg}."}
+    is_strong = re.search(
+        (
+            "(?=^.{%i,}$)"
+            "(?=.*[a-z]{%i,})"
+            "(?=.*[A-Z]{%i})"
+            "(?=.*[0-9]{%i,})"
+            "(?=.*[%s}]+)"
+        ) % 
+        (
+            chars, lowers, uppers,
+            digits, re.escape(string.punctuation)
+        ),
+        passwd
+    )
+
+    return True if is_strong else False
+
+
+print(is_strong_pass("Your pass"))
+
